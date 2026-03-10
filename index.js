13ede97ca73f248505efa5a57f731e55ae32295e
srolket <sveta.shishkina365@gmail.com>
2026-03-10 17:28:27 +0500

    1and2

--- index.js
+++ index.js
@@ -4,19 +4,19 @@ function addListeners() {
     document.getElementById('fadeInPlay')
         .addEventListener('click', function () {
             const block = document.getElementById('fadeInBlock');
-            fadeIn(block, 5000);
+            animaster().fadeIn(block, 5000);
         });
 
     document.getElementById('movePlay')
         .addEventListener('click', function () {
             const block = document.getElementById('moveBlock');
-            move(block, 1000, {x: 100, y: 10});
+            animaster().move(block, 1000, {x: 100, y: 10});
         });
 
     document.getElementById('scalePlay')
         .addEventListener('click', function () {
             const block = document.getElementById('scaleBlock');
-            scale(block, 1000, 1.25);
+            animaster().scale(block, 1000, 1.25);
         });
 }
 
@@ -51,6 +51,24 @@ function move(element, duration, translation) {
 function scale(element, duration, ratio) {
     element.style.transitionDuration =  `${duration}ms`;
     element.style.transform = getTransform(null, ratio);
+}
+
+function animaster() {
+    return {
+        fadeIn: (element, duration) => {
+            element.style.transitionDuration =  `${duration}ms`;
+            element.classList.remove('hide');
+            element.classList.add('show');
+        },
+        move: (element, duration, translation) => {
+            element.style.transitionDuration = `${duration}ms`;
+            element.style.transform = getTransform(translation, null);
+        },
+        scale: (element, duration, ratio) => {
+            element.style.transitionDuration =  `${duration}ms`;
+            element.style.transform = getTransform(null, ratio);
+        }
+    }
 }
 
 function getTransform(translation, ratio) {
